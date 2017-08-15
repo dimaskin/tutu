3bcf95714883a257de75b4eddc55e23422b40283
dimaskin <dimaskin-nv@yandex.ru>
2017-08-15 16:08:26 +0300

    add CRUD methods to controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,2 +1,54 @@
 class RoutesController < ApplicationController
+
+  def index
+    @routes = Route.all
+  end
+
+  def show
+  end
+
+  def new
+    @route = Route.new
+  end
+
+  def edit
+  end
+
+  def create
+    @route = Route.new(route_params)
+    respond_to do |format|
+      if @route.save
+        format.html { redirect_to @route, notice: 'Route was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @route.update(route_params)
+        format.html { redirect_to @route, notice: 'Route was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @route.destroy
+    respond_to do |format|
+      format.html { redirect_to routes_url, notice: 'Route was successfully destroyed.' }
+    end
+  end
+
+  private
+    def set_route
+      @route = Route.find(params[:id])
+    end
+
+    def route_params
+      params.require(:route).permit(:name)
+    end
+
 end
