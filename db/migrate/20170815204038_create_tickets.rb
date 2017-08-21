19c37c47d9fc188de45645f29bf6d44bc77d24db
dimaskin <dimaskin-nv@yandex.ru>
2017-08-21 18:14:00 +0300

    modified migrations

--- db/migrate/20170815204038_create_tickets.rb
+++ db/migrate/20170815204038_create_tickets.rb
@@ -3,7 +3,9 @@ class CreateTickets < ActiveRecord::Migration[5.1]
     create_table :tickets do |t|
       t.integer :user_id
       t.integer :train_id
-      
+      t.integer :start_station_id
+      t.integer :finish_station_id
+
       t.timestamps
     end
   end
